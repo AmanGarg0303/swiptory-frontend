5b2e0d7366fb5a5d6382f99f121783a677ad99e5
Aman <amangarg0303@gmail.com>
2024-04-19 00:43:55 +0530

    navbar changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,39 +2,76 @@ import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import { RegisterModal } from "../modals/registerModal/RegisterModal";
 import { LoginModal } from "../modals/loginModal/LoginModal";
+import { FaBookmark } from "react-icons/fa";
+import { IoMenuSharp } from "react-icons/io5";
 
 export const Navbar = () => {
+  const user = true;
+
   const [openRegisterModal, setOpenRegisterModal] = useState(false);
   const [openLoginModal, setOpenLoginModal] = useState(false);
+  const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
 
+  console.log(openHamburgerMenu);
   return (
     <div>
       <div className={styles.main_container}>
         <h1 className={styles.title}>SwipTory</h1>
+        {!user ? (
+          <div className={styles.btns}>
+            <button
+              onClick={() => setOpenRegisterModal(true)}
+              className={styles.register}
+            >
+              Register Now
+            </button>
+            <RegisterModal
+              openRegisterModal={openRegisterModal}
+              setOpenRegisterModal={setOpenRegisterModal}
+            />
 
-        <div className={styles.btns}>
-          <button
-            onClick={() => setOpenRegisterModal(true)}
-            className={styles.register}
-          >
-            Register Now
-          </button>
-          <RegisterModal
-            openRegisterModal={openRegisterModal}
-            setOpenRegisterModal={setOpenRegisterModal}
-          />
+            <button
+              onClick={() => setOpenLoginModal(true)}
+              className={styles.login}
+            >
+              Sign In
+            </button>
+            <LoginModal
+              openLoginModal={openLoginModal}
+              setOpenLoginModal={setOpenLoginModal}
+            />
+          </div>
+        ) : (
+          <div className={styles.availabeUserBtns}>
+            <div className={styles.btns}>
+              <button className={styles.bookmarks}>
+                <FaBookmark fill="white" />
+                Bookmarks
+              </button>
 
-          <button
-            onClick={() => setOpenLoginModal(true)}
-            className={styles.login}
-          >
-            Sign In
-          </button>
-          <LoginModal
-            openLoginModal={openLoginModal}
-            setOpenLoginModal={setOpenLoginModal}
-          />
-        </div>
+              <button className={styles.addStory}>Add Story</button>
+            </div>
+
+            <img
+              src="https://avatars.githubusercontent.com/u/99589204?v=4"
+              alt=""
+              className={styles.userImg}
+            />
+
+            <IoMenuSharp
+              size={30}
+              className={styles.menuIcon}
+              onClick={() => setOpenHamburgerMenu(!openHamburgerMenu)}
+            />
+            {openHamburgerMenu && (
+              <div className={styles.menuArea}>
+                <h6 className={styles.yourName}>Your Name</h6>
+
+                <button className={styles.logout}>Logout</button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
