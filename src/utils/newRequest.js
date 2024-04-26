faebb6be61aeb4b8d955ffedbae7510fc0f790c4
Aman <amangarg0303@gmail.com>
2024-04-27 00:07:55 +0530

    base url changes

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
 
